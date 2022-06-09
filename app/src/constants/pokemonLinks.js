cf391fcf84a1a11e55e1cf9c87f51404ea35085b
JefferyXie <jeffxie27@outlook.com>
2022-06-09 14:39:57 -0600

    Created pokemon links object.

---
+++ app/src/constants/pokemonLinks.js
@@ -0,0 +1,7 @@
+const pokemonLinks = [
+  "https://pokeapi.co/api/v2/pokemon/1",
+  "https://pokeapi.co/api/v2/pokemon/4",
+  "https://pokeapi.co/api/v2/pokemon/7"
+]
+
+export default pokemonLinks;
