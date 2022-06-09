29cbe0c1b9ad3a14c36de0b9b038fe36035a6bf6
JefferyXie <jeffxie27@outlook.com>
2022-06-09 14:40:15 -0600

    Created pokemon card component.

---
+++ app/src/components/pokemonCard.js
@@ -0,0 +1,21 @@
+// Styles
+import '../styles/pokemonCard.css';
+
+const PokemonCard = ({ pokemonData, select }) => {
+
+  // Updates the selectedPokemon state in pokedexPage to this card's pokemon name.
+  const selectPokemon = (pokemonName) => {
+    select(pokemonName);
+  }
+
+  return (
+    <div className={"card-wrapper " + pokemonData.name + "-spinner"} onClick={() => selectPokemon(pokemonData.name)}>
+      <div className={"flex-col card " + pokemonData.name}>
+        <img className="pokemon-image" src={pokemonData["sprites"]["other"]["official-artwork"]["front_default"]} alt={"An image of " + pokemonData.name}></img>
+        <p className="pokemon-name">{pokemonData.name}</p>
+      </div>
+  </div>
+  )
+}
+
+export default PokemonCard;
